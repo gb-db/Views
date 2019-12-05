ebc00dfe4be47b27e401842c0dffe5354b01338d
gb-db <barsamyang@gmail.com>
2019-12-05 15:05:36 -0500

    Second commit in test project

--- Views.Test/UnitTest1.cs
+++ Views.Test/UnitTest1.cs
@@ -18,13 +18,13 @@ namespace Views.Test
             pr.list = new List<string> { "P1", "P2" };
             Mock<product> mock = new Mock<product>();
             //System.Diagnostics.Debugger.Launch();
-            mock.Setup(m => m.list).Returns(new List<string> { "P1", "P2" });
+            mock.Setup(m => m.list).Returns(new List<string> { "P1", "P2", "P3" });
             // Arrange - create a controller
             HomeController target = new HomeController(mock.Object.list);
             // Action
             List<string> result  = GetViewModel<List<string>>(target.Index());
             // Assert
-            Assert.Equal(2, result.Count);
+            Assert.Equal(3, result.Count);
             //Assert.Equal("P1", result[0].Name);
             //Assert.Equal("P2", result[1].Name);
             //Assert.Equal("P3", result[2].Name);
